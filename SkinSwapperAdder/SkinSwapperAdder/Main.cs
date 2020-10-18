ae638152c030eaf508203f332cb4fb590a497dad
Jipp <59375393+NotJipp@users.noreply.github.com>
2020-10-18 16:04:11 +0200

    Add files via upload

--- SkinSwapperAdder/SkinSwapperAdder/Main.cs
+++ SkinSwapperAdder/SkinSwapperAdder/Main.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
+using System.CodeDom;
 
 namespace SkinSwapperAdder
 {
@@ -75,68 +76,79 @@ namespace SkinSwapperAdder
         {
             richTextBoxOutput.ForeColor = Color.Black;
 
-            if (checkBoxAppend.Checked == true)
+            try
             {
-                if (checkBoxSkinHasFaceAccessories.Checked == false)
+                if (checkBoxAppend.Checked == true)
                 {
-                    if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103)
+                    if (checkBoxSkinHasFaceAccessories.Checked == false)
                     {
-                        richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",";
-                        File.AppendAllText(textBoxFileName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n");
+                        if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103)
+                        {
+                            richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",";
+                            File.AppendAllText(textBoxFileName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Too many characters in one of the fields.");
+                        }
+                    }
+                    else if (checkBoxSkinHasFaceAccessories.Checked == true)
+                    {
+                        if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103 && textBoxSkinFace.Text.Length <= 103)
+                        {
+                            richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",";
+                            File.AppendAllText(textBoxFileName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",\n");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Too many characters in one of the fields.");
+                        }
                     }
                     else
                     {
-                        MessageBox.Show("Too many characters in one of the fields.");
+                        MessageBox.Show("Something went wrong.");
                     }
                 }
-                else if (checkBoxSkinHasFaceAccessories.Checked == true)
-                {
-                    if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103 && textBoxSkinFace.Text.Length <= 103)
-                    {
-                        richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",";
-                        File.AppendAllText(textBoxFileName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",\n");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Too many characters in one of the fields.");
-                    }
-                }
+
                 else
                 {
-                    MessageBox.Show("Something went wrong.");
+                    if (checkBoxSkinHasFaceAccessories.Checked == false)
+                    {
+                        if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103)
+                        {
+                            richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",";
+                            File.WriteAllText(textBoxSkinName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Too many characters in one of the fields.");
+                        }
+                    }
+                    else if (checkBoxSkinHasFaceAccessories.Checked == true)
+                    {
+                        if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103 && textBoxSkinFace.Text.Length <= 103)
+                        {
+                            richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",";
+                            File.WriteAllText(textBoxSkinName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",\n");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Too many characters in one of the fields.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Something went wrong.");
+                    }
                 }
             }
-
-            else
+            finally
             {
-                if (checkBoxSkinHasFaceAccessories.Checked == false)
-                {
-                    if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103)
-                    {
-                        richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",";
-                        File.WriteAllText(textBoxSkinName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Too many characters in one of the fields.");
-                    }
-                }
-                else if (checkBoxSkinHasFaceAccessories.Checked == true)
-                {
-                    if (textBoxSkinBody.Text.Length <= 103 && textBoxSkinHead.Text.Length <= 103 && textBoxSkinFace.Text.Length <= 103)
-                    {
-                        richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",";
-                        File.WriteAllText(textBoxSkinName.Text + ".txt", richTextBoxOutput.Text = "  " + textBoxSkinName.Text + "enabled: \"true\",\n  " + textBoxSkinName.Text + "bodyb: \"" + textBoxSkinBody.Text + "\",\n  " + textBoxSkinName.Text + "headb: \"" + textBoxSkinHead.Text + "\",\n  " + textBoxSkinName.Text + "faceb: \"" + textBoxSkinFace.Text + "\",\n");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Too many characters in one of the fields.");
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Something went wrong.");
-                }
+                textBoxSkinName.Text = "";
+                textBoxSkinBody.Text = "";
+                textBoxSkinHead.Text = "";
+                textBoxSkinFace.Text = "";
+                textBoxCharacters.Text = "";
             }
         }
     }
